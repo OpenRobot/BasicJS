f9dfa985f341c08de2700f8ba8ef659564d52d58
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-07-31 09:20:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js')
 const client = new discord.Client()
 var prefix = "."
-const yt = require('ytdl-core')
+// const yt = require('ytdl-core')
 
 client.on('connect', () => {
     console.log("Connected to Discord but not yet ready.")
