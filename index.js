0d5fa355f42c6de047ff978b8991fe6ceab7b8b9
whyfai <74695568+whyfai@users.noreply.github.com>
2021-09-22 11:24:12 +0700

    Some fixes

--- index.js
+++ index.js
@@ -7,24 +7,30 @@ client.on('connect', () => {
     console.log("Connected to Discord but not yet ready.")
 });
 
-client.on('ready'), () => {
+client.on('ready', () => {
     console.log("Logged in.")
 });
 
 client.on('message', message => {
     if (message.content === `${client.user.tag}`) {
-        message.reply(`Hello ${message.author.tag}`);
+        message.reply(`Hello ${message.author.tag}, my prefix is ${prefix}`);
     }
     if (!message.guild) return;
 
     if (message.content === `${prefix}join`) {
         if (message.author.voice.channel) {
             const connection = await message.author.voice.channel.connect()
-            message.channel.send('Joined VC.');
+            message.channel.send(`Joined ${message.author.voice.channel}`);
         } else {
             message.reply("Connect to a VC First!");
         }
     }
-)};
+    if (message.content === `${prefix}ping`) {
+        message.reply("Pong!")
+    }
+    if (message.content === `${prefix}pong`) {
+        message.reply("Ping!")
+    }
+});
 
 client.login('token')
